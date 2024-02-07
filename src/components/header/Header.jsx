76c3f60c633442274b9cdca219c5e89db6630ef9
pashag123 <pashag123@gmail.com>
2024-02-07 13:08:12 -0800

    maybefix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,24 +2,24 @@
 
 import './Header.css'
 import resume from '../../assets/pasharesume.pdf'
-
+import { Link } from 'react-router-dom'
 function Header() {
     return (
         <aside className='navbar-vertical'>
             <div className='brand-logo'>
-                <a href='/' className='text-light'>Pasha Ghods</a>
+                <Link to='/' className='text-light'>Pasha Ghods</Link>
             </div>
             <nav className='nav flex-column'>
-                <a className='nav-link' href='/'>
+                <Link className='nav-link' to='/'>
                     <i className='fas fa-home'></i> Home
-                </a>
+                </Link>
 
-                <a className='nav-link' href='/contact'>
+                <Link className='nav-link' to='/contact'>
                     <i className='fas fa-envelope'></i> Contact Me
-                </a>
-                <a className='nav-link' href={resume} download>
+                </Link>
+                <Link className='nav-link' to={resume} download>
                     <i className='fas fa-file-alt'></i> My Resume
-                </a>
+                </Link>
 
             </nav>
         </aside>
